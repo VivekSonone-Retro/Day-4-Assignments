719e40e2e813fd3e690dfe6369819fffdf418a53
VivekSonone-Retro <vivek0sonone@gmail.com>
2025-07-27 22:49:41 +0530

    Created file for Assignment6

---
+++ Assignment6.py
@@ -0,0 +1,8 @@
+# Ask the user for a number
+num = int(input("Enter a number: "))
+
+# Check if it's even or odd
+if num % 2 == 0:
+    print("The number is even.")
+else:
+    print("The number is odd.")
