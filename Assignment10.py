2cf1a79d7e6ce03de58f6f0a8290526aa10ac168
VivekSonone-Retro <vivek0sonone@gmail.com>
2025-07-27 22:58:08 +0530

    created a new file for Assignment 10

---
+++ Assignment10.py
@@ -0,0 +1,23 @@
+# Start with a number
+num = 12
+print("Initial value:", num)
+
+# Apply += (Addition Assignment)
+num += 5   # num = num + 5
+print("After += 5:", num)
+
+# Apply -= (Subtraction Assignment)
+num -= 3   # num = num - 3
+print("After -= 3:", num)
+
+# Apply *= (Multiplication Assignment)
+num *= 2   # num = num * 2
+print("After *= 2:", num)
+
+# Apply /= (Division Assignment)
+num /= 4   # num = num / 4
+print("After /= 4:", num)
+
+# Apply %= (Modulus Assignment)
+num %= 3   # num = num % 3
+print("After %= 3:", num)
