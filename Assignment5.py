e5231e25e155f9fdf15eb91c80b0f9a2db1d533a
VivekSonone-Retro <vivek0sonone@gmail.com>
2025-07-27 22:47:11 +0530

    CReated new file for Assignment 5

---
+++ Assignment5.py
@@ -0,0 +1,15 @@
+# Try bitwise operations on small numbers
+a = 5  # Binary: 0101
+b = 3  # Binary: 0011
+
+# Bitwise operations
+print("a & b:", a & b)       # AND
+print("a | b:", a | b)       # OR
+print("a ^ b:", a ^ b)       # XOR
+print("~a:", ~a)             # NOT
+print("a << 1:", a << 1)     # Left shift
+print("b >> 1:", b >> 1)     # Right shift
+
+# Binary representations
+print("Binary of a:", bin(a))
+print("Binary of b:", bin(b))
