1a56b57d4dd224b1d8adc6cfb29fb1c521f53da6
VivekSonone-Retro <vivek0sonone@gmail.com>
2025-07-27 22:39:01 +0530

    Creted file for Assignment 2

---
+++ Assignment2.py
@@ -0,0 +1,11 @@
+# Take two numbers from the user
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+
+# Compare using all comparison operators
+print("a == b:", a == b)
+print("a != b:", a != b)
+print("a > b:", a > b)
+print("a < b:", a < b)
+print("a >= b:", a >= b)
+print("a <= b:", a <= b)
